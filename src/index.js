08af1ed4d0cb17e996dc4d317a6757a8421a7eff
MJ Sung <themrsung@gmail.com>
2023-01-05 22:22:10 +0900

    dd

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@
 //
 // For the backend, we set the container's entrypoint to src/api/index.js
 
-module.exports = require('./www')
+// module.exports = require('./www')
+
+const jsonServer = require("json-server")
+const path = require("path")
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.resolve(__dirname + "/db.json"))
+const middlewares = jsonServer.defaults({
+    static: path.resolve(__dirname + "/../build/")
+})
+
+const port = process.env.PORT || 3001
+
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+
+server.use(router)
+server.listen(port, () => {
+    console.log("JSON Server is running")
+})
